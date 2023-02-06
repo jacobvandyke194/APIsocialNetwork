e5589e44bdbdab248d980953705337d282d7bbb5
Jacob Van Dyke <jacobvandyke194@gmail.com>
2023-02-05 18:15:50 -0700

    finished server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+//boilerpate server
+const express = require("express");
+const db = require("./config/connection");
+const routes = require("./routes");
+const cwd = process.cwd();
+//express
+const PORT = process.env.PORT || 3001;
+const app = express();
+//middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}!`);
+  });
+});
